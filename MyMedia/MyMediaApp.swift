899cc5a96ca02eaa4fad1a0a9cb834a66b2f7c0c
Fabio Rodriguez <fabio.rm93@gmail.com>
2021-10-25 10:29:55 +0200

    Initial Commit

---
+++ MyMedia/MyMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMediaApp.swift
+//  MyMedia
+//
+//  Created by Fabio Rodriguez Martinez on 25/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
