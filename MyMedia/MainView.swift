fb38c8a90b8f25b714ee9f6086b4f554214cc2f2
Fabio Rodriguez <fabio.rm93@gmail.com>
2021-11-04 09:43:25 +0100

    Add buttons + clock

---
+++ MyMedia/MainView.swift
@@ -0,0 +1,158 @@
+//
+//  ContentView.swift
+//  MyMedia
+//
+//  Created by Fabio Rodriguez Martinez on 25/10/2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    //inicializar timer
+    let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
+    //current date y hora
+    @State var currentDate: Date = Date()
+    var dateFormatter: DateFormatter{
+        let formatter = DateFormatter()
+       // formatter.dateStyle = .long
+        formatter.timeStyle = .short
+        return formatter
+    }
+    @State private var isActive : Bool = false
+    var body: some View {
+        NavigationView{
+            VStack{
+                
+                HStack{
+                    Image(systemName: "clock.fill")
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        
+                    Text(dateFormatter.string(from:currentDate))
+                        .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        .foregroundColor(.white)
+                        .lineLimit(1)
+                        .minimumScaleFactor(0.1)
+                        .padding(.top)
+                }
+                
+                .onReceive(timer, perform: { value in
+                    currentDate = value
+                    
+                })
+                ZStack{
+                    GeometryReader{ gr in
+                        VStack{
+                            Spacer()
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.secondary)
+                                .frame(height: gr.size.height * 0.6)
+                                .offset(y :40)
+                            
+                        }
+                        
+                        
+                    }
+                    VStack(){
+                        //reloj
+                        //temperatura
+                        
+                        HStack{
+                            Button(action:{
+                                //TODO ACTION
+                            }){
+                                VStack{
+                                    Image("waze")
+                                        .resizable()
+                                        .aspectRatio(contentMode:.fit)
+                                    Text("Waze")
+                                    
+                                    
+                                }
+                                .font(.title)
+                                .frame(width: 150, height: 100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center)
+                                .padding()
+                                .foregroundColor(.white)
+                                .background(LinearGradient(gradient: Gradient(colors: [Color.blue, Color.blue]), startPoint: .top, endPoint: .bottom))
+                                .cornerRadius(10)
+                            }
+                            Button(action:{
+                                //TODO ACTION
+                            }){
+                                VStack{
+                                    Image("spotify")
+                                        .resizable()
+                                        .aspectRatio(contentMode:.fit)
+                                    Text("Spotify")
+                                    
+                                    
+                                }
+                                    .font(.title)
+                                    .frame(width: 150, height: 100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.green, Color.blue]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(10)
+                            }
+                        }
+                        HStack{
+                            Button(action:{
+                                //TODO ACTION
+                            }){ VStack{
+                                Image("phone")
+                                    .resizable()
+                                    .aspectRatio(contentMode:.fit)
+                                Text("Teléfono")
+                                
+                                
+                            }
+                                    .font(.title)
+                                    .frame(width: 150, height: 100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(10)
+                            }
+                            .frame(height: nil)
+                            Button(action:{
+                                //TODO ACTION
+                            }){
+                                VStack{
+                                   Image("phone")
+                                       .resizable()
+                                       .aspectRatio(contentMode:.fit)
+                                   Text("Teléfono")
+                                   
+                                   
+                               }
+                                    .font(.title)
+                                    .frame(width: 150, height: 100/*@END_MENU_TOKEN@*/, alignment: /*@START_MENU_TOKEN@*/.center)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.orange]), startPoint: .top, endPoint: .bottom))
+                                    .cornerRadius(10)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("MY MEDIA STATION")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading: Image(systemName: "menubar.rectangle"),
+                trailing: Image(systemName: "phone")
+            )
+            .background(Color.secondary)
+            
+        }
+    }
+}
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
